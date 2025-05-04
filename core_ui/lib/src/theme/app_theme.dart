84589b85afb8dbc57acd3138f7b154cf2ccb8a4c
Gemek1 <dzmitry.mikulich@innowise.com>
2025-05-04 23:20:04 +0300

    theme fix

--- core_ui/lib/src/theme/app_theme.dart
+++ core_ui/lib/src/theme/app_theme.dart
@@ -8,6 +8,8 @@ final ThemeData lightTheme = ThemeData.light().copyWith(
   scaffoldBackgroundColor: _appColors.white,
   appBarTheme: _getAppBarTheme(),
   textTheme: _getTextTheme(),
+  textButtonTheme: _getTextButtonTheme(),
+  elevatedButtonTheme: _getElevatedButtonThemeData(),
   inputDecorationTheme: _getInputDecorationTheme(),
   bottomNavigationBarTheme: _getBottomNavigationTheme(),
   primaryColor: _appColors.primaryBg,
@@ -16,6 +18,21 @@ final ThemeData lightTheme = ThemeData.light().copyWith(
     primary: _appColors.primaryBg,
   ),
 );
+
+ElevatedButtonThemeData _getElevatedButtonThemeData() {
+  return ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: WidgetStateProperty.all(_appColors.primaryBg),
+      foregroundColor: WidgetStateProperty.all(_appColors.black),
+      textStyle: WidgetStateProperty.all(AppFonts.normal13),
+      shape: WidgetStateProperty.all(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppDimens.BORDER_RADIUS_12),
+        ),
+      ),
+    ),
+  );
+}
 
 AppBarTheme _getAppBarTheme() {
   return AppBarTheme(
@@ -42,15 +59,25 @@ TextTheme _getTextTheme() {
     titleMedium: AppFonts.normal13,
     bodyMedium: AppFonts.normal13,
     titleLarge: AppFonts.normal20,
+    bodyLarge: AppFonts.normal13,
   ).apply(
     bodyColor: _appColors.black,
     displayColor: _appColors.black,
   );
 }
 
+TextButtonThemeData _getTextButtonTheme() {
+  return TextButtonThemeData(
+    style: ButtonStyle(
+      foregroundColor: WidgetStateProperty.all(_appColors.hyperLinkColor),
+      textStyle: WidgetStateProperty.all(AppFonts.normal13),
+    ),
+  );
+}
+
 InputDecorationTheme _getInputDecorationTheme() {
   return InputDecorationTheme(
-    hintStyle: AppFonts.normal13.copyWith(color: Colors.grey), // ← контрастнее
+    hintStyle: AppFonts.normal13.copyWith(color: Colors.grey),
     border: OutlineInputBorder(
       borderRadius: const BorderRadius.all(
         Radius.circular(AppDimens.BORDER_RADIUS_12),
@@ -87,6 +114,6 @@ InputDecorationTheme _getInputDecorationTheme() {
         width: 2,
       ),
     ),
-    labelStyle: AppFonts.normal13.copyWith(color: Colors.black), // ← читаемо
+    labelStyle: AppFonts.normal13.copyWith(color: Colors.black),
   );
 }
