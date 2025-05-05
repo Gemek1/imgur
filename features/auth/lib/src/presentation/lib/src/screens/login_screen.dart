aaccd6a3453bf8e6addf5f022ffa3d4f2564a341
Gemek1 <dzmitry.mikulich@innowise.com>
2025-05-05 10:26:57 +0300

    name fix

--- features/auth/lib/src/presentation/lib/src/screens/login_screen.dart
+++ features/auth/lib/src/presentation/lib/src/screens/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:navigation/navigation.dart';
 
-import '../auth_bloc/auth_bloc.dart';
+import '../auth_bloc/auth_cubit.dart';
 import 'screens.dart';
 
 @RoutePage<String>()
--- features/auth/lib/src/presentation/lib/src/screens/sign_up_screen_content.dart
+++ features/auth/lib/src/presentation/lib/src/screens/sign_up_screen_content.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../auth_bloc/auth_bloc.dart';
+import '../auth_bloc/auth_cubit.dart';
 
 // TODO(): Change UI according to your requirements
 class SignUpScreenContent extends StatefulWidget {
